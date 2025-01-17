0a077fdbd74c756ab0526d29c6b7beaa1d24f43c
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-01-18 02:13:47 +0530

    Routing & home page done

---
+++ client/src/components/BestSeller.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const BestSeller = () => {
+
+    const { products } = useContext(ShopContext);
+    const [bestSeller, setBestSeller] = useState([]);
+
+    useEffect(() => {
+        const bestProducts = products.filter((item) => (item.bestseller));
+        setBestSeller(bestProducts.splice(0, 5));
+    }, []);
+
+    return (
+        <div className='my-10'>
+            <div className="text-center py-8 text-3xl">
+                <Title text1={'Best'} text2={'sellers'} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Discover our best sellers: timeless favorites loved for their style, quality, and versatility.</p>
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+                {
+                    bestSeller.map((item, index) => (
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default BestSeller
