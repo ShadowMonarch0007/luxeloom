18f21843770087541846eb497a5f20defdab4619
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-01-19 01:46:40 +0530

    cart page done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ShopContext } from '../context/ShopContext';
 const Navbar = () => {
 
     const [visible, setVisible] = useState(false);
-    const { setShowSearch, getCartCount } = useContext(ShopContext);
+    const { setShowSearch, getCartCount, navigate } = useContext(ShopContext);
 
     return (
         <div className="flex items-center justify-between py-5 font-medium">
@@ -34,7 +34,7 @@ const Navbar = () => {
                 </NavLink>
             </ul>
             <div className="flex items-center gap-6">
-                <img onClick={() => setShowSearch(true)} src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
+                <img onClick={() => {setShowSearch(true), navigate('/collection')}} src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
                 <div className="group relative">
                     <img src={assets.profile_icon} alt="" className='w-5 cursor-pointer' />
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -110,12 +110,14 @@ const Collection = () => {
             <option value="high-low">Sort by : High to Low</option>
           </select>
         </div>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
-          {
-            filterProducts.map((item, index) => (
-              <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image} />
-            ))
-          }
+        <div className='overflow-auto h-[80vh]'>
+          <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+            {
+              filterProducts.map((item, index) => (
+                <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image} />
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
