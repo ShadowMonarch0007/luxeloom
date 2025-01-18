62b75a95ce67bbb9930db1fd138d153ffceb09ef
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-01-18 12:36:33 +0530

    collection page done

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,17 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = ({children}) => {
+const ShopContextProvider = ({ children }) => {
 
     const currency = '₹';
     const delivery_fee = 10;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
 
     const value = {
         products,
         currency,
         delivery_fee,
+        search,
+        setSearch,
+        showSearch,
+        setShowSearch,
     }
 
     return (
