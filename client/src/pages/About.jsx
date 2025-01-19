7b013cce6b3a60a1c5eb49467609dd192c086e37
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-01-19 19:19:28 +0530

    about page done

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const About = () => {
   return (
@@ -17,6 +18,24 @@ const About = () => {
           <p>Our mission at Luxeloom is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
         </div>
       </div>
+      <div className='text-xl py-4'>
+        <Title text1={'why'} text2={'choose us'}/>
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Qyality Assurance :</b>
+          <p>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience :</b>
+          <p>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service :</b>
+          <p>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+        </div>
+      </div>
+      <NewsLetterBox/>
     </div>
   )
 }
