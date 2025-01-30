90a53a2848728d3294b03f23eb051b795751eb13
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-01-30 17:54:11 +0530

    add order management functionality with order model, routes, and controller

--- admin/src/pages/Subscriber.jsx
+++ admin/src/pages/Subscriber.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { backendUrl } from '../App';
+import { Trash2, Loader2 } from 'lucide-react';
+import { toast } from 'react-toastify';
 
-const Subscriber = () => {
+const Subscriber = ({ token }) => {
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [removeLoading, setRemoveLoading] = useState(null);
+
+  const fetchList = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`${backendUrl}/api/subscriber/list`, { headers: { token } });
+      if (response.data.success) {
+        setList(response.data.subscribers);
+      }
+    } catch (error) {
+      console.error("Error fetching subscribers:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const removeSubscriber = async (id) => {
+    setRemoveLoading(id);
+    try {
+      const response = await axios.post(`${backendUrl}/api/subscriber/remove`, { id }, { headers: { token } });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList()
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    } finally {
+      setRemoveLoading(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
-    <div>Subscriber</div>
-  )
-}
+    <div>
+      <p className='mb-2 font-semibold'>All Subscribers List</p>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+        <div className='col-span-1 grid grid-cols-[3fr_1fr] items-center py-2 px-4 border bg-gray-100 text-sm font-bold'>
+          <span>Subscribers</span>
+          <span className='text-center'>Action</span>
+        </div>
+        <div className='col-span-1 hidden md:grid grid-cols-[3fr_1fr] items-center py-2 px-4 border bg-gray-100 text-sm font-bold'>
+          <span>Subscribers</span>
+          <span className='text-center'>Action</span>
+        </div>
+        {loading &&
+          Array.from({ length: 5 }).map((_, index) => (
+            <div key={index} className='col-span-1 grid grid-cols-[3fr_1fr] items-center py-2 px-4 border text-sm animate-pulse'>
+              <div className='h-4 w-full bg-gray-300 rounded'></div>
+              <div className='w-full flex items-center justify-center'>
+                <div className='h-6 w-6 bg-gray-300 rounded'></div>
+              </div>
+            </div>
+          ))
+        }
+        {!loading && list.map((subscriber) => (
+          <div key={subscriber._id} className='col-span-1 grid grid-cols-[3fr_1fr] items-center py-2 px-4 border text-sm'>
+            <p className='w-full flex items-center justify-start text-xs md:text-base truncate'>{subscriber.email}</p>
+            <p onClick={() => removeSubscriber(subscriber._id)} className='w-full flex items-center justify-center cursor-pointer'>
+              {removeLoading === subscriber._id ? <Loader2 className='animate-spin' size={24} /> : <Trash2 size={24} />}
+            </p>
+          </div>
+        ))
+        }
+      </div>
+    </div>
+  );
+};
 
-export default Subscriber
+export default Subscriber;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import userRouter from './routes/userRoutes.js'
 import productRouter from './routes/productRoutes.js'
 import subscriberRouter from './routes/subscriberRoutes.js'
 import cartRouter from './routes/cartRoutes.js'
+import orderRouter from './routes/orderRoutes.js'
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -20,6 +21,7 @@ app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/subscriber',subscriberRouter)
 app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
 app.get('/', (req, res) => {
     res.send("API Working")
