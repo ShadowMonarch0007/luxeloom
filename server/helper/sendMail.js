0c54e05f8216ee02a740ab70a83b976995bfd6a2
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-04-28 06:30:56 +0530

    fix to nodemailer integration

--- server/helper/sendMail.js
+++ server/helper/sendMail.js
@@ -1,24 +1,25 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.email",
-  secure: true, // true for port 465, false for other ports
-  auth: {
-    user: "info.luxeloom.shop@gmail.com",
-    pass: process.env.MAIL_PASSWORD, // generated ethereal password
-  },
+    service: "gmail",
+    host: "smtp.gmail.email",
+    secure: true, // true for port 465, false for other ports
+    auth: {
+        user: "info.luxeloom.shop@gmail.com",
+        pass: process.env.MAIL_PASSWORD, // generated ethereal password
+    },
 });
 
 // async..await is not allowed in global scope, must use a wrapper
-async function sendMail(to,subject, text, html) {
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: 'info.luxeloom.shop@gmail.com', // sender address
-    to, // list of receivers
-    subject, // Subject line
-    text, // plain text body
-    html, // html body
-  });
+async function sendMail(to, subject, text, html) {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+        from: 'info.luxeloom.shop@gmail.com', // sender address
+        to, // list of receivers
+        subject, // Subject line
+        text, // plain text body
+        html, // html body
+    });
 }
 
-export {sendMail};
+export { sendMail };
