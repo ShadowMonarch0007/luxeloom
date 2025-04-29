806491cb405356a8ba0918b202e4e98f1cd32568
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-04-30 00:59:40 +0530

    fix: import sendMail helper in userController for email functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import userModel from "../models/userModel.js";
 import validator from "validator";
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
+import { sendMail } from "../helper/sendMail.js";
 
 const createToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET);
